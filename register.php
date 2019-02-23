43ea4c2910a960be864962df93da032a1e6c8928
marioptn <40872854+marioptn@users.noreply.github.com>
2019-02-23 20:11:59 +0100

    Update register.php

--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://amazonreffbot.herokuapp.com/execute.php';
+$WEBHOOK_URL = 'https://amazonreff.herokuapp.com/execute.php';
 $BOT_TOKEN = '713536605:AAFhtoWSPgz54L8f3Ox-BLS6isCxmvqjWnY';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
