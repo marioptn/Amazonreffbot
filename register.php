38b12f1622e932cce8a0bd9329598a810e780823
marioptn <40872854+marioptn@users.noreply.github.com>
2020-12-20 14:35:25 +0100

    Update register.php

--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <?php 
 
 // PARAMETRI DA MODIFICARE 
-$WEBHOOK_URL = 'https://amazonreff.herokuapp.com/register.php';
+$WEBHOOK_URL = 'https://neschyzon.herokuapp.com/register.php';
 $BOT_TOKEN = '1497600949:AAFcjr6iXaOZMBHCVJE_ewnVkSHJ5gfYVeo';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
