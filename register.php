938b487f8d21eb0f1e08ac02c5c43f1e87a5972e
marioptn <40872854+marioptn@users.noreply.github.com>
2021-01-11 12:32:23 +0100

    Update register.php

--- register.php
+++ register.php
@@ -1,17 +1,16 @@
-<?php 
+<?php
 
-// PARAMETRI DA MODIFICARE 
-$WEBHOOK_URL = 'https://neschyzon.herokuapp.com/register.php';
-$BOT_TOKEN = '1509063916:AAHViNPgU3Tk2MEGjjmFPAHq8nGtg5cIbNA';
+// PARAMETRI DA MODIFICARE
+$WEBHOOK_URL = 'https://neschyzon.heroku.com/webhook.php';
+$BOT_TOKEN = '{1509063916:AAHViNPgU3Tk2MEGjjmFPAHq8nGtg5cIbNA}';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
-$API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
-$method = 'setWebhook';
 $parameters = array('url' => $WEBHOOK_URL);
-$url = $API_URL . $method. '?' . http_build_query($parameters);
-$handle = curl_init($url);
-curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
-curl_setopt($handle, CURLOPT_TIMEOUT, 60);
-$result = curl_exec($handle);
-print_r($result);
+$url = \sprintf('https://api.telegram.org/bot%s/setWebhook?%s', $BOT_TOKEN, \http_build_query($parameters));
+$handle = \curl_init($url);
+\curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+\curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
+\curl_setopt($handle, CURLOPT_TIMEOUT, 60);
+$result = \curl_exec($handle);
+\curl_close($handle);
+\print_r($result);
