a3707ac19094bd1dc95a2b81156bfbb6a39147c6
marioptn <40872854+marioptn@users.noreply.github.com>
2021-01-11 11:52:33 +0100

    Update register.php

--- register.php
+++ register.php
@@ -2,7 +2,7 @@
 
 // PARAMETRI DA MODIFICARE 
 $WEBHOOK_URL = 'https://amazonreff.herokuapp.com/register.php';
-$BOT_TOKEN = '713536605:AAF_qo8gGDM2CLsBwUhkq3QzVxmiuTg9BuM';
+$BOT_TOKEN = '1509063916:AAHViNPgU3Tk2MEGjjmFPAHq8nGtg5cIbNA';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
